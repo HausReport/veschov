920e9601bae2482158b0c4fe25e317114ce0d0d1
HausReport <tj_willis@hotmail.com>
2026-01-11 05:36:57 -0500

    Refactor raw damage analysis report

--- src/veschov/pages/5_Raw_Damage_Analysis.py
+++ src/veschov/pages/5_Raw_Damage_Analysis.py
@@ -6,10 +6,11 @@ logger = logging.getLogger(__name__)
 
 import streamlit as st
 
-from veschov.ui.raw_damage_report import render_raw_damage_report
+from veschov.ui.raw_damage_report import RawDamageReport
 
 
 st.set_page_config(page_title="STFC Reports", layout="wide")
 # st.title("Raw Damage by Type")
 
-render_raw_damage_report()
+report = RawDamageReport()
+report.render()
