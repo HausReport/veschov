b2361da40e3c363f685bb00e8365366776bdeaa2
HausReport <tj_willis@hotmail.com>
2026-01-23 19:24:01 -0500

    Add log file explorer report

---
+++ src/veschov/pages/12_Log_File_Explorer.py
@@ -0,0 +1,7 @@
+"""Streamlit page for the Log File Explorer report."""
+
+from __future__ import annotations
+
+from veschov.ui.object_reports.LogFileExplorerReport import render_log_file_explorer_report
+
+render_log_file_explorer_report()
